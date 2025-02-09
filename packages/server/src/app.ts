fd8d8ca6142bbc3644dc418ecaaf8cb4024a9c2b
Stéphane Kapoujyan <stephane.kapoujyan@wizbii.com>
2025-02-09 11:54:37 +0100

    refactor(server): move webserver in a dedicated module

---
+++ packages/server/src/app.ts
@@ -0,0 +1,34 @@
+import { io } from './lib/websocket';
+import { getFrameFromVideoBuffer } from './lib/video';
+import { getCardInfoFromAI } from './lib/ai';
+import { getCardFromScryfall, getSetsFromScryfall } from './lib/mtg';
+import express from 'express';
+import multer from 'multer';
+
+export function webServer(): express.Router {
+  const app = express.Router();
+  const upload = multer({ storage: multer.memoryStorage() });
+
+  app.get('/trigger', (req, res) => {
+    io.emit('clicked');
+    res.status(204).send();
+  });
+
+  app.post('/record', upload.single('file'), async (req, res) => {
+    io.emit('requested');
+    const frameFileName: string = await getFrameFromVideoBuffer((req as any).file.buffer);
+    const info = await getCardInfoFromAI(frameFileName);
+    const json = JSON.parse(info);
+    io.emit('waiting_scryfall');
+    const card = await getCardFromScryfall(json.set, json.collector_number);
+    io.emit('finished', card);
+    res.send(card);
+  });
+
+  app.get('/scryfall-sets', async (req, res) => {
+    const sets = [...new Set(await getSetsFromScryfall())];
+    res.send(sets);
+  });
+
+  return app;
+}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,41 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import { getFrameFromVideoBuffer } from './lib/video';
-import { getCardInfoFromAI } from './lib/ai';
-import { io, startWebsocketConnection } from './lib/websocket';
-import multer from 'multer';
-import { getCardFromScryfall, getSetsFromScryfall } from './lib/mtg';
-
-
-const upload = multer({ storage: multer.memoryStorage() });
+import { startWebsocketConnection } from './lib/websocket';
+import { webServer } from './app';
 
 startWebsocketConnection();
 
 const app = express();
-app.use(cors({
-  origin: '*',
-}));
+app.use(cors({ origin: '*' }));
 
-app.get('/trigger', (req, res) => {
-  io.emit('clicked');
-  res.status(204).send();
-});
-
-app.post('/record', upload.single('file'), async (req, res) => {
-  io.emit('requested');
-  const frameFileName: string = await getFrameFromVideoBuffer((req as any).file.buffer);
-  const info = await getCardInfoFromAI(frameFileName);
-  const json = JSON.parse(info);
-  io.emit('waiting_scryfall');
-  const card = await getCardFromScryfall(json.set, json.collector_number);
-  io.emit('finished', card);
-  res.send(card);
-});
-
-app.get('/scryfall-sets', async (req, res) => {
-  const sets = [...new Set(await getSetsFromScryfall())];
-  res.send(sets);
-})
+app.use('/', webServer());
 
 let port = process.env.WEBSERVER_PORT ?? "3100";
 app.listen(parseInt(port), '0.0.0.0', () => {
