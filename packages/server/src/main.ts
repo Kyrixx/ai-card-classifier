211a41e866edb93900c717796a41df7d5a3e2db0
Stéphane Kapoujyan <stephane.kapoujyan@wizbii.com>
2025-03-15 22:27:55 +0100

    feat(server): add HTTPS support with configurable options

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import * as fs from 'fs';
+import * as https from 'https';
 import { startWebsocketConnection } from './lib/websocket';
 import { webServer } from './app';
+import { parseBoolean } from './lib/utils';
 
 startWebsocketConnection();
 
@@ -11,6 +14,15 @@ app.use(cors({ origin: '*' }));
 app.use('/', webServer());
 
 let port = process.env.WEBSERVER_PORT ?? "3100";
-app.listen(parseInt(port), '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const useHttps = parseBoolean(process.env.USE_HTTPS);
+
+const server = useHttps
+  ? https.createServer({
+      key: fs.readFileSync(process.env.KEY_PATH as string),
+      cert: fs.readFileSync(process.env.CERT_PATH as string),
+    }, app)
+  : app;
+
+server.listen(parseInt(port), '0.0.0.0', () => {
+  console.log(`Server is running on ${useHttps ? "https" : "http"}://localhost:${port}`);
 });
