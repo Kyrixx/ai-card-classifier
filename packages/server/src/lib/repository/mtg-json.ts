191d9879a0183b60f1712491af3780252f7ffa49
Stéphane Kapoujyan <stephane.kapoujyan@wizbii.com>
2025-03-26 19:48:49 +0100

    feat(database): add card management functions and setup database schema

---
+++ packages/server/src/lib/repository/mtg-json.ts
@@ -0,0 +1,26 @@
+import { DatabaseSync } from 'node:sqlite';
+const mtgJsonDb = new DatabaseSync('./src/assets/cards.sqlite');
+const myDb = new DatabaseSync('./src/assets/db.sqlite');
+
+const getCardCountForSetQuery = mtgJsonDb.prepare(`
+select COUNT(distinct cardIdentifiers.scryfallOracleId) as count, cards.rarity from 'cards'
+INNER JOIN cardIdentifiers ON cards.uuid = cardIdentifiers.uuid
+WHERE cards.setCode = ? AND cardIdentifiers.scryfallOracleId IS NOT NULL
+GROUP BY cards.rarity
+`)
+
+const getCardQuery = mtgJsonDb.prepare(`
+SELECT uuid FROM cards
+WHERE setCode = :set AND number = :collectorNumber
+`);
+
+export function getCardCountForSet(set: string) {
+  return getCardCountForSetQuery.all(set);
+}
+
+export function getCards(params: { set: string, collectorNumber: string }[]): string[] {
+  return params.map((param) => {
+    return (getCardQuery.get(param) as string).toString();
+  })
+}
+
---
+++ packages/server/src/lib/repository/my-db.ts
@@ -0,0 +1,30 @@
+import { DatabaseSync } from 'node:sqlite';
+const myDb = new DatabaseSync('./src/assets/db.sqlite');
+
+export function setupUp() {
+  myDb.exec(`
+      create table if not exists cards 
+      (
+          _id       INTEGER   not null /*autoincrement needs PK*/
+              constraint cards_pk
+                  unique,
+          uuid      TEXT,
+          createdAt TIMESTAMP not null,
+          sessionId TEXT,
+          boosterId integer
+      );
+
+      create index cards__id_index
+          on cards (_id);
+
+      create index cards_uuid_index
+          on cards (uuid);
+  `);
+}
+
+const saveCardQuery = myDb.prepare(`
+  INSERT INTO cards (uuid, createdAt, sessionId, boosterId) VALUES (:uuid, NOW(), :sessionId, :boosterId)
+`);
+export function saveCard(params: { uuid: string, sessionId: string, boosterId: number }) {
+  saveCardQuery.run(params);
+}
