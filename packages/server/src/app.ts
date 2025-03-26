579e8b543c867504006a088d2629445566df030c
Stéphane Kapoujyan <stephane.kapoujyan@wizbii.com>
2025-03-26 19:49:09 +0100

    feat(app): refactor routes to use modular endpoint handlers for improved organization

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,45 +1,12 @@
-import { io } from './lib/websocket';
-import { getFrameFromVideoBuffer } from './lib/video';
-import { getCardInfoFromAI } from './lib/ai';
-import { assertCardIsUsable, getCardFromScryfall, getSetsFromScryfall } from './lib/mtg';
 import express from 'express';
-import multer from 'multer';
+import { root, save, set } from './endpoints';
 
 export function webServer(): express.Router {
   const app = express.Router();
-  const upload = multer({ storage: multer.memoryStorage() });
 
-  app.get('/trigger', (req, res) => {
-    io.emit('clicked');
-    res.status(204).send();
-  });
-
-  app.post('/record', upload.single('file'), async (req, res) => {
-    io.emit('requested');
-    const frameFileName: string = await getFrameFromVideoBuffer((req as any).file.buffer);
-    const info = await getCardInfoFromAI(frameFileName);
-    const json = JSON.parse(info);
-    if (json.error) {
-      io.emit('error');
-      res.status(400).send({ error: 'No card detected' });
-      return;
-    }
-    io.emit('waiting_scryfall');
-    const card = await getCardFromScryfall(json.set, json.collector_number);
-    if (!assertCardIsUsable(card)) {
-      io.emit('error');
-      console.log(card);
-      res.status(400).send({ error: 'Card as issues', card });
-      return;
-    }
-    io.emit('finished', card);
-    res.send(card);
-  });
-
-  app.get('/scryfall-sets', async (req, res) => {
-    const sets = [...new Set(await getSetsFromScryfall())];
-    res.send(sets);
-  });
+  app.use('/', root());
+  app.use('/', set());
+  app.use('/', save());
 
   return app;
 }
