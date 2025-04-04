8c0c8cb20f8623245d9aea74dd39db9207875992
Stéphane Kapoujyan <stephane.kapoujyan@wizbii.com>
2025-04-04 22:12:37 +0200

    feat(client): add Deck component and update routing

--- packages/client/src/app.routes.ts
+++ packages/client/src/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DisplayOpeningComponent } from './app/components/display-opening.component';
 import { SessionsComponent } from './app/components/sessions.component';
+import { DeckComponent } from './app/components/deck.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'layout', pathMatch: 'full' },
   { path: 'layout', component: DisplayOpeningComponent },
   { path: 'sessions', component: SessionsComponent },
+  { path: 'deck', component: DeckComponent },
 ];
---
+++ packages/client/src/app/components/deck.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-deck',
+  template: `
+  deck works!
+  `,
+
+})
+export class DeckComponent {
+}
